8c9d963ab1d2d7aedf85c1e96215450ebb8b15e8
iampersistent <develop@zestic.com>
2012-10-27 15:44:16 -0700

    update composer, test bootstrap and travis to use --dev switch in composer to load packages for testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,28 +1,13 @@
 <?php
 
-$vendorDir = __DIR__.'/../vendor';
-require_once $vendorDir.'/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespaces(array(
-    'Molino'                 => array(__DIR__.'/../src', __DIR__),
-    'Symfony'                => $vendorDir.'/symfony/src',
-    'Pagerfanta'             => $vendorDir.'/pagerfanta/src',
-    'Mandango\\Mondator'     => $vendorDir.'/mondator/src',
-    'Mandango'               => $vendorDir.'/mandango/src',
-    'Doctrine\\Common'       => $vendorDir.'/doctrine-common/lib',
-    'Doctrine\\MongoDB'      => $vendorDir.'/doctrine-mongodb/lib',
-    'Doctrine\\ODM\\MongoDB' => $vendorDir.'/doctrine-mongodb-odm/lib',
-    'Doctrine\\DBAL'         => $vendorDir.'/doctrine-dbal/lib',
-    'Doctrine\\ORM'          => $vendorDir.'/doctrine-orm/lib',
-    'Model'                  => __DIR__,
-));
-$loader->registerPrefixes(array(
-    'Twig_' => $vendorDir.'/twig/lib',
-));
-$loader->register();
+if (!@include __DIR__ . '/../vendor/autoload.php') {
+    die(<<<'EOT'
+You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+EOT
+    );
+}
 
 /*
  * Generate Mandango model.
