a31e9704179b050319519b10686e7d1d82e22d17
iampersistent <develop@zestic.com>
2012-10-29 17:56:14 -0700

    get basequery test for Doctrine mongodb passing

---
+++ src/Molino/Doctrine/ODM/MongoDB/BaseQuery.php
@@ -0,0 +1,196 @@
+<?php
+
+/*
+ * This file is part of the Molino package.
+ *
+ * (c) Pablo Díez <pablodip@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Molino\Doctrine\ODM\MongoDB;
+
+use Molino\BaseQuery as BaseBaseQuery;
+use Doctrine\ODM\MongoDB\Query\Builder;
+
+/**
+ * The base query for Doctrine MongoDB ODM.
+ *
+ * @author Pablo Díez <pablodip@gmail.com>
+ */
+abstract class BaseQuery extends BaseBaseQuery
+{
+    private $queryBuilder;
+    private $lastParameterId = 0;
+
+    /**
+     * Returns the query builder.
+     *
+     * @return \Doctrine\ODM\MongoDB\Query\Builder The query builder.
+     */
+    public function getQueryBuilder()
+    {
+        if (null === $this->queryBuilder) {
+            $queryBuilder = $this
+                ->getMolino()
+                ->getDocumentManager()
+                ->createQueryBuilder($this->getModelClass())
+            ;
+            $this->configureQueryBuilder($queryBuilder);
+            $this->queryBuilder = $queryBuilder;
+        }
+
+        return $this->queryBuilder;
+    }
+
+    /**
+     * Method to configure the query builder.
+     *
+     * @param Doctrine\ODM\MongoDB\Query\Builder The query builder.
+     */
+    protected function configureQueryBuilder(Builder $queryBuilder)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filterEqual($field, $value)
+    {
+        $this->andWhere('equals', $field, $value);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filterNotEqual($field, $value)
+    {
+        $this->andWhere('notEqual', $field, $value);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filterLike($field, $value)
+    {
+        $this->andWhere('equals', $field, new \MongoRegex($this->buildLikePattern($value)));
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filterNotLike($field, $value)
+    {
+        $this->andWhere('notEqual', $field, new \MongoRegex($this->buildLikePattern($value)));
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filterIn($field, array $values)
+    {
+        $this->andWhere('in', $field, $values);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filterNotIn($field, array $values)
+    {
+        $this->andWhere('notIn', $field, $values);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filterGreater($field, $value)
+    {
+        $this->andWhere('gt', $field, $value);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filterLess($field, $value)
+    {
+        $this->andWhere('lt', $field, $value);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filterGreaterEqual($field, $value)
+    {
+        $this->andWhere('gte', $field, $value);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filterLessEqual($field, $value)
+    {
+        $this->andWhere('lte', $field, $value);
+
+        return $this;
+    }
+
+    private function andWhere($comparison, $field, $value)
+    {
+        $this->getQueryBuilder()->field($field)->$comparison($value);
+    }
+
+    private function buildLikePattern($value)
+    {
+        $start = false;
+        $pattern = '';
+        $end = false;
+
+        $parsed = $this->parseLike($value);
+        reset($parsed);
+        if ('*' !== current($parsed)) {
+            $start = true;
+        }
+        end($parsed);
+        if ('*' !== current($parsed)) {
+            $end = true;
+        }
+
+        foreach ($parsed as $v) {
+            if ('*' === $v) {
+                $pattern .= '.*';
+            } else {
+                $pattern .= $v;
+            }
+        }
+
+        if ($start) {
+            $pattern = '^'.$pattern;
+        }
+        if ($end) {
+            $pattern .= '$';
+        }
+
+        $pattern = '/'.$pattern.'/';
+
+        return $pattern;
+    }
+}
---
+++ tests/Molino/Tests/Doctrine/ODM/MongoDB/SelectQueryTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Molino\Tests\Doctrine\ODM\MongoDB;
+
+use Molino\Doctrine\ODM\MongoDB\Molino;
+use Molino\Doctrine\ODM\MongoDB\SelectQuery;
+use Doctrine\ODM\MongoDB\QueryBuilder;
+
+class SelectQueryTest extends TestCase
+{
+    private $query;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->molino = new Molino($this->documentManager);
+        $this->modelClass = 'Model\Doctrine\ODM\MongoDB\Article';
+        $this->query = new SelectQuery($this->molino, $this->modelClass);
+    }
+
+    public function testConfigureQueryBuilder()
+    {
+        $this->assertSame(QueryBuilder::SELECT, $this->query->getQueryBuilder()->getType());
+    }
+
+    public function testFields()
+    {
+        $this->assertSame($this->query, $this->query->fields(array('foo', 'bar')));
+        $this->assertSame('SELECT m.foo, m.bar FROM Model\Doctrine\ODM\MongoDB\Article m', $this->query->getQueryBuilder()->getDQL());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testFieldsNotString()
+    {
+        $this->query->fields(array('foo', 12));
+    }
+
+    public function testSort()
+    {
+        $this->assertSame($this->query, $this->query->sort('title'));
+        $this->assertSame('SELECT m FROM Model\Doctrine\ODM\MongoDB\Article m ORDER BY m.title ASC', $this->query->getQueryBuilder()->getDQL());
+    }
+
+    public function testSortAsc()
+    {
+        $this->assertSame($this->query, $this->query->sort('title', 'asc'));
+        $this->assertSame('SELECT m FROM Model\Doctrine\ODM\MongoDB\Article m ORDER BY m.title ASC', $this->query->getQueryBuilder()->getDQL());
+    }
+
+    public function testSortDesc()
+    {
+        $this->assertSame($this->query, $this->query->sort('title', 'desc'));
+        $this->assertSame('SELECT m FROM Model\Doctrine\ODM\MongoDB\Article m ORDER BY m.title DESC', $this->query->getQueryBuilder()->getDQL());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSortOrderNotValid()
+    {
+        $this->query->sort('name', 'no');
+    }
+
+    public function testLimit()
+    {
+        $this->assertSame($this->query, $this->query->limit(10));
+        $this->assertSame(10, $this->query->getQueryBuilder()->getMaxResults());
+    }
+
+    public function testSkip()
+    {
+        $this->assertSame($this->query, $this->query->skip(20));
+        $this->assertSame(20, $this->query->getQueryBuilder()->getFirstResult());
+    }
+
+    public function testAll()
+    {
+        // cannot mock Doctrine\ODM\MongoDB\Query because it's declared as final
+
+        $articles = $this->loadArticles(10);
+        $this->assertSame($articles, $this->query->all());
+    }
+
+    public function testOne()
+    {
+        $articles = $this->loadArticles(10);
+        $this->assertSame($articles[0], $this->query->one());
+    }
+
+    public function testOneNull()
+    {
+        $this->assertNull($this->query->one());
+    }
+
+    public function testCount()
+    {
+        $this->loadArticles(10);
+        $this->assertSame(10, $this->query->count());
+    }
+
+    public function testGetIterator()
+    {
+        $articles = $this->loadArticles(10);
+        $iterator = $this->query->getIterator();
+        $this->assertInstanceOf('Traversable', $iterator);
+        $this->assertSame($articles, iterator_to_array($iterator));
+    }
+
+    public function testCreatePagerfantaAdapter()
+    {
+        $adapter = $this->query->createPagerfantaAdapter();
+        $this->assertInstanceOf('Pagerfanta\Adapter\DoctrineORMAdapter', $adapter);
+        $this->assertEquals($this->query->getQueryBuilder()->getQuery(), $adapter->getQuery());
+    }
+}
